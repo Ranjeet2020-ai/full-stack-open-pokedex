360bac9c7139f417c199ed4e9c8fb26f85438fef
Ranjeet Rajbanshi <Ranjeet.rajbanshi@mettropolia.fi>
2026-04-28 15:36:45 +0300

    simulate broken health check

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const app = express()
 
-// get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
